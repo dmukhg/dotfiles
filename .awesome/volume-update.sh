7345bd9e3a3147ee11ae96df01d4514c7cc53375
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2011-09-01 00:02:05 +0530

    Added xmodmap

--- .awesome/volume-update.sh
+++
@@ -1,3 +0,0 @@
-volume=`amixer -c $cardid -- sget $channel | pcregrep 'Left:.+[0-9]+%' | pcregrep -o '[0-9]+%' | pcregrep -o '[0-9]+'`
-
-echo "0 widget_tell $widget `handle \"$volume\"`" | awesome-client
