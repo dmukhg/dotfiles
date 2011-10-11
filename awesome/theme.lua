205088217bbf1925a2fee1efaec30986b5b3fa95
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2011-10-11 11:02:48 +0530

    awesome theme: highlight current window and screen
    Signed-off-by: Dipanjan Mukherjee <dipanjan.mu@gmail.com>

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -15,17 +15,17 @@ theme.font      = "sans 8"
 
 -- {{{ Colors
 theme.fg_normal = "#DCDCCC"
-theme.fg_focus  = "#F0DFAF"
+theme.fg_focus  = "#eeeeee"
 theme.fg_urgent = "#CC9393"
 theme.bg_normal = "#3F3F3F"
-theme.bg_focus  = "#1E2320"
+theme.bg_focus  = "#1F82BF"
 theme.bg_urgent = "#3F3F3F"
 -- }}}
 
 -- {{{ Borders
 theme.border_width  = "2"
 theme.border_normal = "#3F3F3F"
-theme.border_focus  = "#0066ff"
+theme.border_focus  = "#1F82BF"
 theme.border_marked = "#CC9393"
 -- }}}
 
